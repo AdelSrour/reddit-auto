279e21f63105cc57d55ba705709d2989d1dac3ca
AdelSrour <adelmalys@hotmail.com>
2026-05-29 19:29:10 +0300

    feat: create index file for automation components export
    - Added an index file in the automation directory to streamline exports of key components including SubredditSelector, InstanceCard, InstanceForm, AvailablePostsTable, ScheduleModal, ScheduledRepliesTable, and CompletedRepliesTable.
    - This change enhances modularity and simplifies imports for other parts of the application.

---
+++ src/components/automation/index.ts
@@ -0,0 +1,7 @@
+export { SubredditSelector } from './SubredditSelector';
+export { InstanceCard } from './InstanceCard';
+export { InstanceForm } from './InstanceForm';
+export { AvailablePostsTable } from './AvailablePostsTable';
+export { ScheduleModal } from './ScheduleModal';
+export { ScheduledRepliesTable } from './ScheduledRepliesTable';
+export { CompletedRepliesTable } from './CompletedRepliesTable';

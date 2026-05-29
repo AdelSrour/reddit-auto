7315bdbdc0334d2bd6d518b500827e3ded1ea540
AdelSrour <adelmalys@hotmail.com>
2026-05-29 19:29:58 +0300

    feat: add additional hooks for automation instances
    - Exported new hooks: useAutomationInstances, useAutomationInstance, and useInstanceForm from the hooks index file to enhance functionality and modularity in managing automation instances.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,6 @@
 export { useAccounts } from './useAccounts';
 export { useAccount } from './useAccount';
 export { useF5botMatches } from './useF5botMatches';
+export { useAutomationInstances } from './useAutomationInstances';
+export { useAutomationInstance } from './useAutomationInstance';
+export { useInstanceForm } from './useInstanceForm';
